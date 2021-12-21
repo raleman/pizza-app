6cc9f1d19f94b405ba9762487e8e3cac1371e74d
Rick Aleman <rick@doctorrick.com>
2021-12-21 01:57:13 -0500

    login and logout added to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Menu, Icon, Container } from 'semantic-ui-react';
+import { Menu, Icon, Container, Button } from 'semantic-ui-react';
 import { formatPrice } from '../helpers';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class NavBar extends Component{
 
@@ -11,10 +12,28 @@ class NavBar extends Component{
   }
 
   render(){
+
+    const {
+      isLoading,
+      isAuthenticated,
+      error,
+      user,
+      loginWithRedirect,
+      logout,
+    } = this.props.auth0;
+
     return(
       <Menu fixed='top' borderless id='navbar'>
         <Container>
           <Menu.Item as={Link} to='/menu' id='navbar-header'>Pizza 42</Menu.Item>
+          <Menu.Item id='navbar-login' position='right'>
+          {!isAuthenticated && (
+            <Button color='teal' onClick={loginWithRedirect}>Log in</Button>
+          )}
+          {isAuthenticated && (
+            <Button color='teal' onClick={logout}>Log out</Button>
+          )}
+          </Menu.Item>       
           <Menu.Item id='navbar-total' position='right'>
             <Icon name='cart'/>
             {formatPrice(this.props.orderTotal)}
@@ -26,4 +45,4 @@ class NavBar extends Component{
   }
 }
 
-export default NavBar;
+export default withAuth0(NavBar);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-rlndq9pe.us.auth0.com"
+    clientId="cgvRs9b5Dnql5uaEn0GDHuUANMNKytSH"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
